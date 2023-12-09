d30e407896c7a0bfe9aade1510df1fa8aa98caeb
AMarianss <andrea12m19@gmail.com>
2023-12-09 19:36:06 +0100

    aggiungiPersone added

--- src/main/java/ispw/OptimisticCoverage.java
+++ src/main/java/ispw/OptimisticCoverage.java
@@ -1,8 +1,33 @@
 package ispw;
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class OptimisticCoverage {
-    public static void main(String[] args) {
 
+
+import java.util.Scanner;
+
+public class OptimisticCoverage {
+
+    private String[] nomiPersone;
+
+    public void aggiungiPersone() {
+
+        Scanner scanner = new Scanner(System.in);
+        int i = 1;
+        String buff;
+        while (true) {
+            System.out.println("Scrivi il nome della persona da voler aggiungere alla posizione" + i + "(esc se uscire): ");
+            buff = scanner.nextLine();
+            if (buff.equals("esc")) {
+                System.out.println("Hai finito di inserire i nomi");
+                break;
+            } else {
+                nomiPersone[i - 1] = buff;
+            }
+        }
+    }
+    public static void main(String[] args){
+        OptimisticCoverage me = new OptimisticCoverage();
+        me.aggiungiPersone();
+        for (int i = 0; i < me.nomiPersone.length; i++) {
+            System.out.println(me.nomiPersone[i]);
+        }
     }
 }
